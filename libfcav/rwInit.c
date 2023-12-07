6d56d0e23753139db4202896390c4608e5d27b5a
Hao Liang <lianghao597032692@qq.com>
2023-12-07 16:42:19 +0800

    init

--- libfcav/rwInit.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "ulibuser.h"
-
-// int main(int argc, char* argv[])
-// {
-//     rw_Initial();
-// }
--- main.cpp
+++ main.cpp
@@ -1,66 +1,99 @@
-#include <libfcav/fcReceiver.h>
+#include <fcntl.h>
+#include <linux/videodev2.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
 #include <unistd.h>
 #include <glview.hpp>
-#include <libfcav/fcUtils.h>
-#include <libfcav/ctlCam.h>
+
 
 unsigned char *rgbBuffer;
-unsigned port_id = 0x334400;
-unsigned win_id = 25;
-unsigned tgt_id = 10;
 unsigned w = 1920;
 unsigned h = 1080;
+int fd; // 文件描述符，用于V4L2操作
 
-void loop(int)
-{
-    GLViewer::getInstance()->paint_gl();
-    FCReceiver::getInstance()->getFrame(rgbBuffer);
-    GLViewer::getInstance()->update(w, h, rgbBuffer);
-    glutTimerFunc(1000 / 25, loop, 0);
+struct buffer {
+    void   *start;
+    size_t  length;
 };
+buffer *buffers;
+
+bool init_camera(const char *dev_name) {
+    fd = open(dev_name, O_RDWR | O_NONBLOCK, 0);
+    if (fd < 0) {
+        perror("无法打开设备");
+        return false;
+    }
+
+    // 此处省略了检查和设置摄像头格式等代码,请根据实际情况自行添加
+
+    // 启动捕获
+    enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (-1 == ioctl(fd, VIDIOC_STREAMON, &type))
+        perror("VIDIOC_STREAMON");
+
+    return true;
+}
+
+bool capture_image() {
+    // 队列缓冲区
+    struct v4l2_buffer buf;
+    CLEAR(buf);
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = 0;
+
+    if (-1 == ioctl(fd, VIDIOC_QBUF, &buf))
+        perror("VIDIOC_QBUF");
+
+    // 取出缓冲区
+    if (-1 == ioctl(fd, VIDIOC_DQBUF, &buf)) {
+        switch (errno) {
+            case EAGAIN:
+                return false;
+            case EIO:
+                // 忽略EIO错误
+            default:
+                perror("VIDIOC_DQBUF");
+        }
+    }
+
+    assert(buf.index < 2);
+
+    // 此处可以对获取的图像数据进行处理，例如格式转换等
+    memcpy(rgbBuffer, buffers[buf.index].start, buf.bytesused);
+
+    return true;
+}
+
+void close_camera() {
+    // 关闭流
+    enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (-1 == ioctl(fd, VIDIOC_STREAMOFF, &type))
+        perror("VIDIOC_STREAMOFF");
+
+    // 关闭设备
+    close(fd);
+}
+
+
+
+// OpenGL的循环函数，替换原有的loop函数
+void loop(int) {
+    if (capture_image()) {
+        GLViewer::getInstance()->update(w, h, rgbBuffer);
+        GLViewer::getInstance()->paint_gl();
+    }
+    glutTimerFunc(1000 / 25, loop, 0);
+}
 
 int main(int argc, char **argv)
 {
-    setenv("MWV206_GLVERSION", "1", 1);
+    setenv("MWV207_GLVERSION", "1", 1);
     setenv("vblank_mode", "1", 1);
 
-    rgbBuffer = new unsigned char[w * h * 2];
-
-    // check fc driver
-    if (get_mod_status() != 0)
-    {
-        // insmod
-        if (insmod_fc_driver() != 0)
-        {
-            printf("insmod fc driver failed\n");
-            return -1;
-        }
-    }
-
-    // check port_login
-    char reg_path[256];
-    if (get_fc_pci_reg_path(reg_path) == 0)
-    {
-        int linkStatus = port_login(reg_path, 0, port_id);
-        if (linkStatus != 0)
-        {
-            printf("port_login failed once, try again\n");
-            linkStatus = port_login(reg_path, 0, port_id);
-            if (linkStatus == -1)
-            {
-                sleep(5);
-                if (linkStatus == -1)
-                {
-                    printf("port_login failed twice, exit\n");
-                    return -1;
-                }
-            }
-        }
-    }
-    else
-    {
-        printf("get fc pci reg path failed\n");
-        return -1;
+    rgbBuffer = new unsigned char[w * h * 3];
+    if (!init_camera("/dev/video0")) {
+        return 1;
     }
 
     glutInit(&argc, argv);
@@ -69,30 +102,16 @@ int main(int argc, char **argv)
     // glutInitDisplayMode(GLUT_DOUBLE); //设置双缓冲模式，即带有front buffer 和 back buffer
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
     glutInitWindowSize(1280, 720);
-    glutCreateWindow("FCAV");
+    glutCreateWindow("AV");
 
     GLViewer::getInstance()->init_gl();
 
-    FCReceiver *fcReceiver = FCReceiver::getInstance();
-    fcReceiver->init(port_id, win_id, w, h, 16, FCReceiver::FC_AV_RGB);
-
-    fcReceiver->start();
-
-    // start camera
-    int camera_ret = start_cam(port_id, tgt_id, win_id, w, h, 0x80780438);
-    if (camera_ret != 0)
-    {
-        printf("start camera failed\n");
-        return -1;
-    }
-    printf("start camera success\n");
 
     glutDisplayFunc(GLViewer::getInstance()->paint_gl);
     glutTimerFunc(1000 / 25, loop, 0);
     glutMainLoop();
 
-    stop_cam(port_id);
+    close_camera();
     delete[] rgbBuffer;
-    fcReceiver->stop();
     return 0;
 }
